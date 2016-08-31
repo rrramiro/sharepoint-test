c3a6767232db3a63b96e588990233da744c7de47
rrramiro <ramiro.calle@rpharms.com>
2016-08-31 17:08:36 +0100

    Reorganise directory structure

--- src/main/scala/com/jw/sharepoint/examples/DispatchHttpAuthClientsAsync.scala
+++ src/main/scala/com/jw/sharepoint/examples/DispatchHttpAuthClientsAsync.scala
@@ -1,20 +1,17 @@
 package com.jw.sharepoint.examples
 
-import java.util.Properties
 import com.ning.http.client.Realm.{AuthScheme, RealmBuilder}
-import scala.concurrent.duration.Duration
 import scalaxb.HttpClientsAsync
 import scala.concurrent._, duration._
 
 trait DispatchHttpAuthClientsAsync extends HttpClientsAsync{
-  val properties: Properties
+  val user: String
+  val password: String
+  val hostname: String
+  val requestTimeout = 60 seconds
+  val connectionTimeout = 5 seconds
 
-  lazy val httpClient = new DispatchHttpClient {}
-
-  def requestTimeout: Duration = 60.seconds
-  def connectionTimeout: Duration = 5.seconds
-
-  trait DispatchHttpClient extends HttpClient {
+  lazy val httpClient = new HttpClient {
     import dispatch._, Defaults._
     lazy val http = Http.configure {
       _.setRequestTimeout(requestTimeout.toMillis.toInt)
@@ -23,9 +20,6 @@ trait DispatchHttpAuthClientsAsync extends HttpClientsAsync{
     }
 
     def request(in: String, address: java.net.URI, headers: Map[String, String]): Future[String] = {
-      val user = properties.getProperty("username")
-      val password = properties.getProperty("password")
-
       val (domain, principal) = user.split('\\').toList match {
         case d :: p :: Nil => d -> p
         case _ => "" -> user
@@ -36,10 +30,10 @@ trait DispatchHttpAuthClientsAsync extends HttpClientsAsync{
         .setPrincipal(principal)
         .setNtlmDomain(domain)
         .setPassword(password)
-        .setNtlmHost("infopoint")
+        .setNtlmHost(hostname)
         .build()
 
-      val req: Req = url(address.toString).setBodyEncoding("UTF-8").setRealm(realm) <:< headers << in
+      val req = url(address.toString).setBodyEncoding("UTF-8").setRealm(realm) <:< headers << in
 
       http(req > as.String)
     }
